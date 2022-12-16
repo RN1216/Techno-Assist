f9c309b720618978b5297c25002c2a663f274be7
RN1216 <reunnoor@gmail.com>
2022-12-16 20:47:45 +0600

    first commit

--- src/Routers/Routers/Routers.js
+++ src/Routers/Routers/Routers.js
@@ -81,6 +81,10 @@ const router = createBrowserRouter([
           </AdminRouters>
         ),
       },
+      {
+        path:'*',
+        element:<ErrorPage></ErrorPage>
+    }
     ],
   },
 ]);
